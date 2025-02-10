1668fbf7d4846563209fd3f6fbc8bb1e9a605824
miskinole2002 <sadeufotsing2002@gmail.com>
2025-02-10 16:59:34 -0500

    installation dependance

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,6 @@
-print("hello le groupe")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn  #pour demarrer le serveur
+import snowflake.connector as sc
+from dotenv import load_dotenv # pip install python-dotenv  c'est la commande pour creer la commande d'environnement 
+
